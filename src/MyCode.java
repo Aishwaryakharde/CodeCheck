d589787660dfbbbfe2d6dbc367f7abfb6ecbe8a5
aishwarya <aishukharde991@gmail.com>
2023-04-16 19:39:09 -0700

    Added Code

---
+++ src/MyCode.java
@@ -0,0 +1,13 @@
+
+public class MyCode {
+
+	
+	public static void code() {
+		for(int i=1;i<=55;i++) {
+		System.out.println("Thank you god for keeping me and my family happy and healthy.");
+	}
+}
+	public static void main(String[] args) {
+		code()
+;	}
+}
